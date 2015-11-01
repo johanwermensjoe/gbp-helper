a191418c0f2006662fbaa8f3c4ed7160a03ad094
johanwermensjoe <johanwermensjoe@gmail.com>
2015-11-01 20:33:05 +0100

    Still converting to python

--- gbp-helper.py
+++ gbp-helper.py
@@ -4,13 +4,17 @@ import argparse
 import os
 import shutil
 import re
+import subprocess
+import yaml
 
 __version__ = "0.2"
 
 ## Constants
+PIPE = subprocess.PIPE
 DEFAULT_CONFIG_PATH = "./gbp-helper.conf"
+BUILD_DIR = "../build-area"
 MASTER_BRANCH = "master"
-EX_CONFIG = 
+EX_CONFIG = \
     "## gbp-helper.conf: $(basename $(pwd))\n\n" + \
     "#[REQUIRED]\n\n" + \
     "releaseBranch=master\n" + \
@@ -26,7 +30,7 @@ EX_CONFIG =
 ########################## Argument Parsing #############################
 #########################################################################
 
-
+'''
 # Start
 parser = argparse.ArgumentParser(description='Helps maintain debian packeges with git.')
 
@@ -53,6 +57,42 @@ parser.add_argument('--config', \
 parser.add_argument('dir', nargs='?', default=os.getcwd())
     
 args = parser.parse_args()
+'''
+
+# Start
+parser = argparse.ArgumentParser(description='Helps maintain debian packeges with git.')
+subparsers = parser.add_subparsers(help='sub-command help')
+parser.add_argument('--version', '-V', action='store_true', \
+    help='shows the version')
+parser.add_argument('--create-config', action='store_true', \
+    help='creates an example gbp-helper.conf file')
+
+# The commit-release subcommand.
+parser_r.add_argument('commit-release', action='store_true', \
+    help='commits the latest release to upstream and merges with debian branch')
+parser_r.add_argument('--undo', '-z', action='store_true', \
+    help='undo the latest release commit (rollback upstream and debian branches)')
+parser_r.add_argument('--verbose', '-v', action='store_true', \
+    help='enables verbose mode')
+parser_r.add_argument('--safemode', '-s', action='store_true', \
+    help='disables any file changes')
+parser_r.add_argument('--config', \
+    help='path to the gbp-helper.conf file')
+parser_r.add_argument('dir', nargs='?', default=os.getcwd())
+
+# The test-build subcommand.
+parser_t.add_argument('test-build', action='store_true', \
+    help='builds the latest debian commit')
+
+# The commit-build subcommand.
+parser_b.add_argument('--commit-build', '-b', action='store_true', \
+    help='builds and tags the latest debian commit')
+
+# The upload-build subcommand.
+parser_u.add_argument('--upload-build', '-u', action='store_true', \
+    help='uploads the latest build to the configured ppa')
+
+args = parser.parse_args()
 
 ############################ Build Tools ################################
 #########################################################################
@@ -68,99 +108,122 @@ def printMsg(msg, priority=0):
     if priority > 1 or args.verbose:
         print msg
 
+# Executes a shell command given as a list of the command followed by the arguments.
+# Errors will be raised as CommandError.
+# Returns the command output.
+def execCmd(cmd):
+    process = subprocess.Popen(cmd, stdout=PIPE, stderr=PIPE)
+    stdoutput, stderroutput = process.communicate()
+
+    if 'fatal' in stdoutput:
+        # Handle error case
+        raise CommandError(stderroutput) #TODO
+    else:
+        # Success!
+        return stdoutput
+
 # Checks if the current directory is a git repository.
-# Returns 1 if an error occured and prints message.
-def is_git_rep(): #TODO
+# Returns False if the current directory isn't a git repository.
+def is_git_rep():
     try:
-        git status >/dev/null 2>&1
+        execCmd(["git", "status"])
         return True
-    except:
+    except CommandError as e: #TODO
         return False
 
-# Switches to git branch:
-# Returns 1 if an error occured and prints message.
+# Switches to git branch.
+# Errors will be raised as GitError (if checkout isn't possible).
 def switch_branch(branchName):
     # Verify that the current dir is a git repository.
     if is_git_rep():
-        # Try to switch branch.
         try:
-            (git checkout $1 >/dev/null 2>&1 && return 0) #TODO
+            # Try to switch branch.
+            execCmd(["git", "checkout", branchName])
         except:
-            raise Exception(branchName, "Please make sure that the branch " \
-                                        "<" + branchName + "> exists and all changes are commited"
+            raise GitError(branchName, "Please make sure that the branch \'" + \
+                                        branchName + "\' exists and all changes are commited") #TODO
     else:
-        raise Exception(pwd + " or " + arg.dir, \
-                            "The current directory is not a git repository")
+        raise GitError(pwd + " or " + arg.dir, \
+                            "The current directory is not a git repository") #TODO
 
 # Retrives the tags for the latest commit (HEAD):
-# Returns 1 if an error occured or no tags exist and prints message.
+# Errors will be raised as GitError (underlying errors or if no tags exists).
+# Returns the list of HEAD tags for the given branch.
 def get_head_tags(branchName):
     switch_branch(branchName)
-    headTags=$(git tag --points-at HEAD) #TODO
-    
-    # Check that some tags exists.    
-    if not headTags:
-        raise Exception(branchName, "The HEAD on branch <$1> has no tags")
-    
-    return headTags
+    try: 
+        headTags = execCmd(["git", "tag", "--points-at", "HEAD"])
+
+        # Check that some tags exists.    
+        if not headTags: #TODO OK?
+            raise GitError("The HEAD on branch \'" + branchName + "\' has no tags") #TODO
+        return headTags
+    except CommandError as e:
+        raise GitError("The tags pointing at \'" + branchName + "\' HEAD, could not be retrived") #TODO
 
 # Retrives the latest HEAD tag (for tags: <tag_type>/<version>) for a branch. 
+
 def get_head_tag(branchName, tagType):
     # Get the latest HEAD tags.
     headTags = get_head_tags(branchName)
         
     # Find the matching tags.
-    matchingTags = re.match(r'''^tagType/*$''', headTags) #TODO multiline?
+    matchingTags = re.findall(r"(?m)^" + tagType + r"/.*$", headTags)
 
     # Make sure atleast some tag follows the right format.
     if matchingTags:
-        # Find the "latest tag". #TODO
+        # Find the "latest tag" 
+        # Assuming format: <pkg_name>/<upstream_version>~<deb_version>
+        matchingTags.sort(key=lambda s: map(int, s.split('/')[1].split('~')[0].split('.')))
+        return matchingTags[0]
     else:
-        raise Exception(headTags, "The HEAD on branch \'" + branchName + \
-                            "\' has no tags of type: " tagType + "/<version>")
+        raise GitError("The HEAD on branch \'" + branchName + \
+                            "\' has no tags of type: " + tagType + "/<version>") #TODO
 
 # Retrives the HEAD tag version (for tags: <tag_type>/<version>) for a branch: 
 def get_tag_version(branchName, tagType):
     # Get the latest HEAD tag.
-    headTag = get_head_tag(branchName, tagType) #TODO
+    headTag = get_head_tag(branchName, tagType)
     
     # Get the version part of the tag.
-    tagVersion = re.match(r'''^tagType/(*$)''', headTag) #TODO
+    tagVersion = re.match(r"^" + tagType + r"/(.*$)", headTag)
 
     if tagVersion:
-        return tagVersion
+        return tagVersion.group(1)
     else:
-        raise Exception(headTag, "A tag version could not be extracted")
+        raise GitError("A tag version could not be extracted") #TODO
 
 # Checks whether the first version string is greater than or equal to the second: 
-def is_version_lte(v1, v2):  #TODO
-    [ "$1" = "`echo -e "$1\n$2" | sort -V | head -n1`" ]
-
-# Checks whether the first version string is greater than the second.
-def is_version_lt(v1, v2):  #TODO
-    [ "$1" = "$2" ] && return 1 || return is_version_lte $1 $2
+def is_version_lte(v1, v2):
+    versions = [v1, v2]
+    matchingTags.sort(key=lambda s: map(int, s.split('~')[0].split('.')))
+    return matchingTags[0] == v1
 
 # Cleans or if not existant creates a directory.
+# Prints progress messages.
 def clean_dir(dirPath):
     remove_dir(dirPath)
     os.makedirs(dirPath)    
 
 # Removes a directory.
+# Prints progress messages.
 def remove_dir(dirPath):
     if os.path.isdir(dirPath):
         # Remove directory recursively.
         shutil.rmtree(dirPath)
 
 # Cleans the default build directory and switches to the release branch.
+# Prints progress messages.
 def prepare_build():
     # Make sure we are on the debian branch.
     printMsg("Switching to debian branch: <$debianBranch>")
     switch_branch(debianBranch)
 
     printMsg("Cleaning old build")
-    clean_dir(buildDir)
+    clean_dir(BUILD_DIR)
 
 # Creates an example gbp-helper.conf file.
+# Prints progress messages.
 def create_ex_config(configPath):
     # Make sure file does not exist.
     if os.path.exists(configPath):
@@ -175,47 +238,58 @@ def create_ex_config(configPath):
         except IOError as e:
             printMsg("I/O error({0}): {1}".format(e.errno, e.strerror), 1)
             
+# Asks a yes/no question via raw_input() and return their answer.
+# "question" the string presented, "default" is the presumed answer 
+# if the user just hits <Enter>. It must be "yes" (the default), 
+# "no" or None (meaning an answer is required of the user).
+# Returns True if user answers "yes", else False.
+def prompt_user_yn(question, default="yes"):
+    valid = {"yes": True, "y": True, "ye": True,
+             "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
 
-# Asks for user confirmation [y/N].
-def prompt_user_yn(promptMsg): #TODO
-    read -r -p promptMsg + " [y/N] " response
-    case $response in
-        [yY][eE][sS]|[yY]) 
-            return 0
-            ;;
-        *)
-            return 1
-            ;;
-    esac
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = raw_input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "
+                             "(or 'y' or 'n').\n")
 
 # Update the config variables.
-def update_config_vars(configPath): #TODO
-    printMsg("Reading config file")
+# Errors will be rised by ConfException
+# Prints progress messages.
+def update_config_vars(configPath):
     # Switch branch to master before trying to read config.
-    switch_branch $masterBranch
-    eval $(sed '/=/!d;/^ *#/d;s/=/ /;' < "$1" | while read -r key val
-    do
-        str="$key='$val'"
-        echo "$str"
-    done)
+    switch_branch(MASTER_BRANCH)
 
-# Updates standard global build variables (release version, package name etc.) and
-# values from config file.
+    # Parse config file.
+    printMsg("Reading config file")
+    config = yaml.safe_load(open(configPath))
+    
+    # Make sure the required values are set. #TODO
+    
+
+# Updates standard global build variables (package name and config values).
 # After a successful run it will have (besides setting the variables):
-#   - Ensured that the current dir is a git repository.
-#   - Switched to the release branch.
-#   - Ensured that the latest release commit is propperly tagged.
 def update_build_vars(): #TODO
     # Update from config.    
-    update_config_vars $configPath
+    update_config_vars(configPath)
 
     printMsg("Setting build variables")
     # Get package name from repository name (parent dir name).
-    packageName=$(basename $(pwd))
-
-    # Set build paths.
-    tmpPath="/tmp/" + packageName
-    buildDir="../build-area"
+    global packageName
+    packageName = execCmd(["basename", "`pwd`"]) #TODO varList + cmd ok?
 
 
 ######################### Command Execution #############################
@@ -231,7 +305,7 @@ if args.version:
 
 # Create example config.
 if args.create_config: #TODO
-    create_ex_config $configPath
+    create_ex_config(args.config if args.config else DEFAULT_CONFIG_PATH)
     # Always exit after config creation.
     quit()
 
@@ -243,17 +317,36 @@ if args.undo_release: #TODO
     # Update the build variables.
     update_build_vars()
     
-    # Find out what the latest merge commit between upstream and debian branches.
+    try:
+        # Find out what the latest merge commit between upstream and debian branches.
+        # TODO
+
+        # Reset debian to the previous commit to the merge.
+        switch_branch(debianBranch)
+        try:
+            execCmd(["git", "reset", "--hard", "HEAD~1"])
+        except CommandError as e:
+            printMsg("The debian branch <" + debianBranch + "> could not be reset" + \
+                        "to before the last upstream merge.", 1)
     
-    # Reset debian to the previous commit to the merge.
-    switch_branch $debianBranch
-    git reset --hard HEAD~1
-    
-    # Reset upstream to the previous commit to its HEAD.
-    upstreamTag=get_tag $upstreamBranch $upstreamTagType
-    switch_branch $upstreamBranch
-    git reset --hard HEAD~1
-    git tag -d $upstreamTag
+        # Reset upstream to the previous commit to its HEAD.
+        upstreamTag = get_tag(upstreamBranch, upstreamTagType)
+        switch_branch(upstreamBranch)
+        try:
+            execCmd(["git", "reset", "--hard", "HEAD~1"])
+        except CommandError as e:
+            printMsg("The upstream branch <" + upstreamBranch + "> could not be reset" + \
+                        "to before the last upstream commit.", 1)
+        
+        # Remove the latest upstream tag.        
+        try:
+            execCmd(["git", "tag", "-d", upstreamTag])
+        except CommandError as e:
+            printMsg("The latest tag (" + upstreamTag + ") on upstream branch <" + \
+                        upstremBranch + "> cound not be deleted", 1)
+    except GitError as e:
+        # Print the error. #TODO
+        printMsg(e.msg, 1)
     
     # Always exit after creation.
     quit()
@@ -272,15 +365,20 @@ if args.upload_build: #TODO
         quit()
 
     # Make sure that the latest debian commit is tagged.
-    debianTagVersion=get_tag_version "debian" $debianTagType ||
-        echo $debianTagVersion
+    try:
+        debianTagVersion = get_tag_version(debianBranch, debianTagType)
+    except GitError as e:
+        printMsg(e.msg, 1)
         printMsg("The latest debian commit isn't porperly tagged, run gbp-helper -b", 1)
-        quit
+        quit()
 
     # Set the name of the .changes file and upload.
-    dput "ppa:" + ppaName + " ../build-area/" + packageName + "_" + \
-        debianTagVersion + "_source.changes"
-    
+    try:
+        execCmd(["dput", "ppa:" + ppaName, "../build-area/" + packageName + "_" + \
+                                            debianTagVersion + "_source.changes"])
+    except CommandError as e:
+        printMsg("The package could not be uploaded to ppa:" + ppaName, 1)
+
     # Always exit after creation.
     quit()
 
@@ -291,18 +389,23 @@ if args.commit_release: #TODO
 
     # Constants
     printMsg("Setting sourcedir")
+    tmpPath="/tmp/" + packageName    
     sourceDirName = packageName + "-" + releaseVersion
-    sourceDirPath = os.path.join(tmpPath, sourceDirName)s
+    sourceDirPath = os.path.join(tmpPath, sourceDirName)
     tarPath = os.path.join(tmpPath, packageName + "_" + releaseVersion + ".orig.tar.gz")
     
     # Get the tagged version from the release branch.
-    releaseVersion = get_tag_version(releaseBranch, releaseTagType) ||
-    	(echo $releaseVersion; exit 1)
+    try:
+        releaseVersion = get_tag_version(releaseBranch, releaseTagType)
+        upstreamVersion = get_tag_version(upstreamBranch, upstreamTagType)
 
-    # Check that the release version is greater than the upstream version.
-    upstreamVersion = get_tag_version(upstreamBranch, upstreamTagType) && 
-    	is_version_lte(releaseVersion, upstreamVersion) && 
-            (printMsg("Error: Release version is less than upstream version, aborting", 1); quit()) || \
+        # Check that the release version is greater than the upstream version.
+        if not is_version_lte(releaseVersion, upstreamVersion):
+            raise GitError("Release version is less than upstream version, aborting")
+    except GitError as e:
+        printMsg(e.msg, 1)
+        quit()
+
                 (echo $upstreamVersion; printMsg("No upstream version detected, asuming 0", 1); quit())
 
     # Clean build directory.
@@ -347,7 +450,7 @@ if args.test_build: #TODO
     # Build and without tagging and do linthian checks.
     printMsg("Building debian package")
     gbp buildpackage --git-debian-branch=$debianBranch --git-upstream-branch=$upstreamBranch \
-      --git-export-dir=$buildDir --git-ignore-new --git-builder="debuild -S" \
+      --git-export-dir=$BUILD_DIR --git-ignore-new --git-builder="debuild -S" \
       --git-postbuild='echo "Running Lintian..."' && lintian -I $GBP_CHANGES_FILE && echo "Lintian OK"
 
 # Commit build.
@@ -366,12 +469,59 @@ if args.commit_build: #TODO
         printMsg("Your gpg key id is not set in your gbp-helper.conf, disabling tag signing.", 1)
         gbp buildpackage --git-tag \
             --git-debian-branch=$debianBranch --git-upstream-branch=$upstreamBranch \
-            --git-export-dir=$buildDir --git-builder="debuild -S" \
+            --git-export-dir=$BUILD_DIR --git-builder="debuild -S" \
             --git-postbuild='echo "Running Lintian..."' \
                 && lintian -I $GBP_CHANGES_FILE && echo "Lintian OK"
     else
         gbp buildpackage --git-tag  --git-sign-tags --git-keyid=$gpgKeyId \
             --git-debian-branch=$debianBranch --git-upstream-branch=$upstreamBranch \
-            --git-export-dir=$buildDir --git-builder="debuild -S" \
+            --git-export-dir=$BUILD_DIR --git-builder="debuild -S" \
             --git-postbuild='echo "Running Lintian..."' \
                 && lintian -I $GBP_CHANGES_FILE && echo "Lintian OK"
+
+######################### Errors / Exceptions ###########################
+#########################################################################
+
+class Error(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+class CommandError(Error):
+    """Exception raised for errors in the input.
+
+    Attributes:
+        expr -- input expression in which the error occurred
+        msg  -- explanation of the error
+    """
+
+    def __init__(self, expr, msg):
+        self.expr = expr
+        self.msg = msg
+
+class ConfigError(Error):
+    """Exception raised for errors in the input.
+
+    Attributes:
+        expr -- input expression in which the error occurred
+        msg  -- explanation of the error
+    """
+
+    def __init__(self, expr, msg):
+        self.expr = expr
+        self.msg = msg
+
+
+class GitError(Error):
+    """Raised when an operation attempts a state transition that's not
+    allowed.
+
+    Attributes:
+        prev -- state at beginning of transition
+        next -- attempted new state
+        msg  -- explanation of why the specific transition is not allowed
+    """
+
+    def __init__(self, prev, next, msg):
+        self.prev = prev
+        self.next = next
+        self.msg = msg
